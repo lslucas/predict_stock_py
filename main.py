b90698adc49d0a82f9709d45e27477162f3dc7c3
Lucas Serafim <lslucas@gmail.com>
2017-12-26 22:03:56 -0600

    Renamed predict_stock to main

--- main.py
+++ main.py
@@ -1,36 +1,22 @@
-import os
 import sys
-import tweepy
 import requests
 import numpy as np
+from inc import twitter
 
 from keras.models import Sequential
 from keras.layers import Dense
 from textblob import TextBlob
 
-
-# First we login into twitter
-consumer_key = ''
-consumer_secret = ''
-access_token = ''
-access_token_secret = ''
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-user = tweepy.API(auth)
-
 # Where the csv file will live
-FILE_NAME = 'historical.csv'
-
+FILE_NAME = 'data/historical.csv'
 
 def stock_sentiment(quote, num_tweets):
-    # Checks if the sentiment for our quote is
-    # positive or negative, returns True if
-    # majority of valid tweets have positive sentiment
-    list_of_tweets = user.search(quote, count=num_tweets)
+    list_of_tweets = twitter.q(quote, num_tweets)
     positive, null = 0, 0
 
     for tweet in list_of_tweets:
         blob = TextBlob(tweet.text).sentiment
+
         if blob.subjectivity == 0:
             null += 1
             next
@@ -42,16 +28,18 @@ def stock_sentiment(quote, num_tweets):
 
 
 def get_historical(quote):
-    # Download our file from google finance
-    url = 'http://www.google.com/finance/historical?q=NASDAQ%3A'+quote+'&output=csv'
+    url = 'https://app.quotemedia.com/quotetools/getHistoryDownload.csv?&webmasterId=501&startDay=02&startMonth=02&startYear=2002&endDay=02&endMonth=12&endYear=2017&isRanged=false&symbol=' + quote
     r = requests.get(url, stream=True)
 
-    if r.status_code != 400:
+    if r.status_code == 200:
         with open(FILE_NAME, 'wb') as f:
             for chunk in r:
                 f.write(chunk)
 
         return True
+    else:
+        print('Could not load csv with financial data: ' + url)
+        sys.exit()
 
 
 def stock_prediction():
@@ -70,7 +58,7 @@ def stock_prediction():
     def create_dataset(dataset):
         dataX = [dataset[n+1] for n in range(len(dataset)-2)]
         return np.array(dataX), dataset[2:]
-        
+
     trainX, trainY = create_dataset(dataset)
 
     # Create and fit Multilinear Perceptron model
@@ -78,31 +66,31 @@ def stock_prediction():
     model.add(Dense(8, input_dim=1, activation='relu'))
     model.add(Dense(1))
     model.compile(loss='mean_squared_error', optimizer='adam')
-    model.fit(trainX, trainY, nb_epoch=200, batch_size=2, verbose=2)
+    model.fit(trainX, trainY, batch_size=2, epochs=500, verbose=0)
 
     # Our prediction for tomorrow
     prediction = model.predict(np.array([dataset[0]]))
-    result = 'The price will move from %s to %s' % (dataset[0], prediction[0][0])
+    result = 'The price of ' + stock_quote + ' will move from %s to %s' % (dataset[0], prediction[0][0])
 
     return result
 
-    
+
 # Ask user for a stock quote
-stock_quote = raw_input('Enter a stock quote from NASDAQ (e.j: AAPL, FB, GOOGL): ').upper()
+stock_quote = input('Enter a stock quote from NASDAQ (e.j: AAPL, FB, GOOGL): ').upper()
 
 # Check if the stock sentiment is positve
-if not stock_sentiment(stock_quote, num_tweets=100):
-    print 'This stock has bad sentiment, please re-run the script'
-    sys.exit()
+if not stock_sentiment(stock_quote, num_tweets=500):
+    print('This stock has bad sentiment, please re-run the script')
+    #sys.exit()
 
 # Check if we got te historical data
 if not get_historical(stock_quote):
-    print 'Google returned a 404, please re-run the script and'
-    print 'enter a valid stock quote from NASDAQ'
+    print('Google returned a 404, please re-run the script and')
+    print('enter a valid stock quote from NASDAQ')
     sys.exit()
 
 # We have our file so we create the neural net and get the prediction
-print stock_prediction()
+print(stock_prediction())
 
 # We are done so we delete the csv file
 os.remove(FILE_NAME)
